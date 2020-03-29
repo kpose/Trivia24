a53fd761239113f1a2bde7eb34665cf9cb96d213
kpose <ganihujude@gmail.com>
2020-03-29 05:55:49 +0100

    add server files

--- server/server.js
+++ server/server.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 const lodash = require('lodash');
+
+//build server and hook socket
+const io = require('socket.io')(require('http').createServer(function(){}).listen(80));
+
+const players = { };
+let inProgress = false;
