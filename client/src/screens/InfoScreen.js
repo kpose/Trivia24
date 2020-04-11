eb2fa6145eab0606fe526b5e713cc356b6040e4c
kpose <ganihujude@gmail.com>
2020-04-11 01:34:12 +0100

    resolve

--- client/src/screens/InfoScreen.js
+++ client/src/screens/InfoScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 function InfoScreen() {
   return (
@@ -12,15 +12,14 @@ function InfoScreen() {
 export default InfoScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+
+  outerContainer : {
+    flex : 1,
+    alignItems : "center",
+    justifyContent : "center",
     backgroundColor: '#1F2026'
   },
-  text: {
-    color: '#ffffff',
-    fontSize: 24,
-    fontWeight: 'bold'
-}
-})
+
+  
+
+}); 
